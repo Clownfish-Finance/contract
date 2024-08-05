86bfbbf39c63c0e067b3bd4680b917cb7d0b59c3
cxp-13 <lantianlaoli@gmail.com>
2024-08-05 22:26:50 +0800

    add CreditLogic;

--- contracts/credit/CreditManager.sol
+++ contracts/credit/CreditManager.sol
@@ -1,8 +1,16 @@
 pragma solidity ^0.8.24;
 
 import "../interfaces/ICreditManager.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import {UNDERLYING_TOKEN_MASK, BitMask} from "../libraries/BitMask.sol";
+import {CreditLogic} from "../libraries/CreditLogic.sol";
 
 
 contract CreditManager is ICreditManager {
-
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using BitMask for uint256;
+    using Math for uint256;
+    using CreditLogic for CollateralDebtData;
+   
 }
--- contracts/interfaces/ICreditManager.sol
+++ contracts/interfaces/ICreditManager.sol
@@ -1,6 +1,17 @@
 pragma solidity ^0.8.24;
 
 
+struct CollateralDebtData {
+    uint256 debt;                           
+    uint256 cumulativeIndexNow;            
+    uint256 cumulativeIndexLastUpdate;    
+    uint256 accruedInterest;               
+    uint256 accruedFees;                   
+    uint256 totalValue;                    
+}
+
+
+
 interface ICreditManager  {
     
 }
